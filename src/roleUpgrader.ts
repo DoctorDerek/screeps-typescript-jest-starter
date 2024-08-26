944ace5d1a9bc1117f83a7eef8730d08f08de2f4
Dr. Derek Austin <derekraustin@gmail.com>
2024-08-25 21:08:17 -0600

    fix(upgraders): upgraders were bugged to always refetch instead of using their load up

--- src/roleUpgrader.ts
+++ src/roleUpgrader.ts
@@ -11,30 +11,26 @@ interface UpgraderMemory extends CreepMemory {
 
 const roleUpgrader = {
   run: function (creep: Upgrader) {
-    if (
-      creep.memory.upgrading &&
-      creep.store[RESOURCE_ENERGY] < creep.store.getCapacity()
-    ) {
-      creep.memory.upgrading = false
+    const isFull = creep.store.getFreeCapacity() === 0
+    const isEmpty = creep.store[RESOURCE_ENERGY] === 0
+    if (isEmpty) creep.memory.upgrading = false
+    if (!creep.memory.upgrading && !isFull) {
+      actionFillUp(creep)
       creep.say("⚡ pick up")
     }
-    if (!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
+    if (!creep.memory.upgrading && isFull) {
       creep.memory.upgrading = true
       creep.say("⚡ upgrade")
     }
 
-    if (creep.memory.upgrading) {
-      if (
-        creep.room.controller &&
-        creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE
-      ) {
-        creep.moveTo(creep.room.controller, {
-          visualizePathStyle: { stroke: "#ffffff" }
-        })
-      }
-    } else {
-      actionFillUp(creep)
-    }
+    if (
+      creep.memory.upgrading &&
+      creep.room.controller &&
+      creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE
+    )
+      creep.moveTo(creep.room.controller, {
+        visualizePathStyle: { stroke: "#ffffff" }
+      })
   }
 }
 
