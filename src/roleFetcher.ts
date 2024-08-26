728fc003eadab22fe1f3ca9631a9d79b850758ec
Dr. Derek Austin <derekraustin@gmail.com>
2024-08-26 17:35:05 -0600

    feat(fetchers): also pick up from container resources not just dropped resources

--- src/roleFetcher.ts
+++ src/roleFetcher.ts
@@ -32,14 +32,17 @@ const roleFetcher = {
         thisCreep.memory.depositTargetNumber = null
         thisCreep.memory.mission = "PICK UP"
       }
-    } else {
-      if (thisCreep.store.getUsedCapacity() >= carryingCapacity) {
+    } else if (thisCreep.memory.mission === "PICK UP") {
+      const isFull = thisCreep.store.getUsedCapacity() >= carryingCapacity
+      if (isFull) {
         // We can clear our marker of which resource we were gathering
         thisCreep.memory.droppedResourceNumber = null
         thisCreep.memory.objective = null
         // And go to to drop off resources
         thisCreep.memory.mission = "DEPOSIT"
       } else {
+        // Fetch dropped resources priority over container resources
+
         /* TODO: Fix fetcher competition logic, based e.g. on miner logic
       // Get all the fetchers who have assigned objectives
 
@@ -85,6 +88,12 @@ const roleFetcher = {
         })*/
         // Target 1x carryingCapacity, i.e. full loads only */
 
+        const containers = thisCreep.room.find(FIND_STRUCTURES, {
+          filter: (structure) =>
+            structure.structureType == STRUCTURE_CONTAINER &&
+            structure.store.getUsedCapacity() >= carryingCapacity
+        }) as unknown as StructureContainer[]
+
         if (droppedResources.length) {
           if (thisCreep.memory.droppedResourceNumber == null) {
             // Decide on current droppedResource assignment by dividing
@@ -121,7 +130,7 @@ const roleFetcher = {
               droppedResources[thisCreep.memory.droppedResourceNumber]
             ) == ERR_NOT_IN_RANGE
           ) {
-            thisCreep.say(`🛍️ move`)
+            thisCreep.say(`🛍️ moveDrop`)
             thisCreep.moveTo(
               droppedResources[thisCreep.memory.droppedResourceNumber],
               { visualizePathStyle: { stroke: "#ffaa00" } }
@@ -135,6 +144,58 @@ const roleFetcher = {
             // Maybe we already picked it up, or someone else did
             thisCreep.memory.droppedResourceNumber = null
             thisCreep.memory.objective = null
+          }
+        } else if (containers.length > 0) {
+          if (thisCreep.memory.depositTargetNumber == null) {
+            // Decide on current container assignment by dividing
+            // the amount of container resources by the distance to them:
+            let bestIndex = 0
+            const bestContainer = containers.reduce((a, b) => {
+              const aDistance = thisCreep.pos.getRangeTo(a)
+              const bDistance = thisCreep.pos.getRangeTo(b)
+              if (
+                a.store.getUsedCapacity() / aDistance >
+                b.store.getUsedCapacity() / bDistance
+              ) {
+                bestIndex = containers.indexOf(a)
+                return a
+              } else {
+                bestIndex = containers.indexOf(b)
+                return b
+              }
+            })
+            thisCreep.memory.depositTargetNumber = bestIndex
+            thisCreep.say(`🛍️ ${bestContainer.store.getUsedCapacity()}`)
+            console.log(
+              `${thisCreep.name} assigned to @containers[${thisCreep.memory.depositTargetNumber}]`
+            )
+            console.log(
+              `${bestContainer.store.getUsedCapacity()} container resources at ${
+                bestContainer.pos.x
+              },${
+                bestContainer.pos.y
+              } with a distance of ${thisCreep.pos.getRangeTo(bestContainer)}`
+            )
+          }
+          if (
+            thisCreep.withdraw(
+              containers[thisCreep.memory.depositTargetNumber],
+              RESOURCE_ENERGY
+            ) == ERR_NOT_IN_RANGE
+          ) {
+            thisCreep.say(`🛍️ moveCont`)
+            thisCreep.moveTo(containers[thisCreep.memory.depositTargetNumber], {
+              visualizePathStyle: { stroke: "#ffaa00" }
+            })
+          }
+          if (
+            thisCreep.withdraw(
+              containers[thisCreep.memory.depositTargetNumber],
+              RESOURCE_ENERGY
+            ) == ERR_INVALID_TARGET
+          ) {
+            // Maybe we already picked it up, or someone else did
+            thisCreep.memory.depositTargetNumber = null
           }
         } else {
           // Explore
