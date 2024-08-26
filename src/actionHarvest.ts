b300e1a1b6caa07c7047061ded367c8d6416fa83
Dr. Derek Austin <derekraustin@gmail.com>
2024-08-26 15:32:38 -0600

    fix(harvesters): deprecate the source number in harvesters because it conflicts with miners

--- src/actionHarvest.ts
+++ src/actionHarvest.ts
@@ -3,56 +3,68 @@ import type { Harvester } from "roleHarvester"
 
 function actionHarvest(creep: Harvester, harvesters: Harvester[]) {
   const freeCapacity = creep.store.getFreeCapacity()
-  const hasRoom = freeCapacity > 0
   const isFull = freeCapacity === 0
-  let sources = creep.room.find(FIND_SOURCES_ACTIVE)
-  // .filter((source) => source.energy > 0)
-  if (hasRoom && creep.memory.sourceNumber == null) {
-    // Go harvest active resources
-    // Grab the used sourceNumbers (indices) from the other harvesters
-    const usedSourceNumbers = harvesters
-      .filter((harvester) => harvester.memory.sourceNumber != null)
-      .map((harvester) => harvester.memory.sourceNumber)
+  // let sources = creep.room.find(FIND_SOURCES_ACTIVE)
+  // // .filter((source) => source.energy > 0)
+  // // if (hasRoom && creep.memory.sourceNumber == null) {
+  // //   // Go harvest active resources
+  // //   // Grab the used sourceNumbers (indices) from the other harvesters
+  // //   const usedSourceNumbers = harvesters
+  // //     .filter((harvester) => harvester.memory.sourceNumber != null)
+  // //     .map((harvester) => harvester.memory.sourceNumber)
 
-    // Sort sources by distance from this creep
-    sources = sources.sort(
-      (a, b) =>
-        creep.pos.getRangeTo(a.pos.x, a.pos.y) -
-        creep.pos.getRangeTo(b.pos.x, b.pos.y)
-    )
+  // //   // Sort sources by distance from this creep
+  // //   sources = sources.sort(
+  // //     (a, b) =>
+  // //       creep.pos.getRangeTo(a.pos.x, a.pos.y) -
+  // //       creep.pos.getRangeTo(b.pos.x, b.pos.y)
+  // //   )
 
-    // Assign an unused source to this creep
-    creep.memory.sourceNumber = sources.findIndex(
-      (source, sourceIndex) =>
-        // Not assigned to another harvester and reachable by this creep
-        !usedSourceNumbers.includes(sourceIndex) &&
-        creep.pos.findPathTo(source.pos)?.length > 0
-    ) // || Math.floor(Math.random() * sources.length) // Fall back to random
+  // //   // Assign an unused source to this creep
+  // //   creep.memory.sourceNumber = sources.findIndex(
+  // //     (source, sourceIndex) =>
+  // //       // Not assigned to another harvester and reachable by this creep
+  // //       !usedSourceNumbers.includes(sourceIndex) &&
+  // //       creep.pos.findPathTo(source.pos)?.length > 0
+  // //   ) // || Math.floor(Math.random() * sources.length) // Fall back to random
 
-    creep.say("🌾 assign")
-    console.log(
-      `🌾 assign: ${creep.name} assigned to @sources[${creep.memory.sourceNumber}]`
-    )
-  }
+  // //   creep.say("🌾 assign")
+  // //   console.log(
+  // //     `🌾 assign: ${creep.name} assigned to @sources[${creep.memory.sourceNumber}]`
+  // //   )
+  // // }
 
-  if (hasRoom && creep.memory.sourceNumber != null) {
-    if (creep.harvest(sources[creep.memory.sourceNumber]) == ERR_NOT_IN_RANGE) {
-      creep.say("🌾 move")
-      if (
-        creep.moveTo(sources[creep.memory.sourceNumber], {
-          visualizePathStyle: { stroke: "#ffaa00" }
-        }) != OK
-      )
-        creep.memory.sourceNumber = null // Maybe I can't reach it? ERR_NO_PATH?
+  // // Find closest source
+
+  // if (hasRoom && creep.memory.sourceNumber != null) {
+  //   if (creep.harvest(sources[creep.memory.sourceNumber]) == ERR_NOT_IN_RANGE) {
+  //     creep.say("🌾 move")
+  //     if (
+  //       creep.moveTo(sources[creep.memory.sourceNumber], {
+  //         visualizePathStyle: { stroke: "#ffaa00" }
+  //       }) != OK
+  //     )
+  //       creep.memory.sourceNumber = null // Maybe I can't reach it? ERR_NO_PATH?
+  //   }
+  //   if (creep.harvest(sources[creep.memory.sourceNumber || 0]) === OK) {
+  //     // Log destination while harvesting
+  //     creep.memory.destination = { x: creep.pos.x, y: creep.pos.y }
+  //     creep.say("🌾 harvest")
+  //   }
+  // }
+
+  // Since harvesters conflict with miners so much I deprecated the sourceNumber
+  if (!isFull) {
+    const target = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE)
+    if (target) {
+      const result = creep.harvest(target)
+      if (result == OK) creep.say("🌾 harvest")
+      if (result == ERR_NOT_IN_RANGE) {
+        creep.say("🌾 move")
+        creep.moveTo(target)
+      }
     }
-    if (creep.harvest(sources[creep.memory.sourceNumber || 0]) === OK) {
-      // Log destination while harvesting
-      creep.memory.destination = { x: creep.pos.x, y: creep.pos.y }
-      creep.say("🌾 harvest")
-    }
-  }
-
-  if (isFull) {
+  } else if (isFull) {
     // Deposit if full
     creep.memory.destination = null
     creep.say("🌾 deposit")
