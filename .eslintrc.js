5f685d0a323b55a3ef3e96b108fc91359a6669cd
Dr. Derek Austin <derekraustin@gmail.com>
2024-08-25 20:58:50 -0600

    chore(ESLint): disable ESLint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,7 +32,7 @@ module.exports = {
     ],
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-use-before-define": "off",
-    "typescript-eslint/explicit-module-boundary-types": "off"
+    "@typescript-eslint/explicit-module-boundary-types": "off"
   },
   overrides: [
     {
