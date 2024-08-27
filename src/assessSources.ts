cd80b831a746dfd3d4883293f405f5606fe1c568
Dr. Derek Austin <derekraustin@gmail.com>
2024-08-27 14:42:23 -0600

    fix(miners): miners should delete the spots when they get assigned so they don't get multiple miners assigned to the same spot in one tick

--- src/assessSources.ts
+++ src/assessSources.ts
@@ -6,34 +6,32 @@ export default function assessSources(
   thisCreep: Miner,
   availableMiningPositions: MineablePositions
 ) {
-  if (availableMiningPositions.size === 0) {
-    // No available mining positions
-    // --> Mission: EXPLORE
-    thisCreep.memory.mission = "EXPLORE"
-    thisCreep.say("⛏️ EXPLORE!!")
-  } else {
-    // Found at least 1 available mining position
-    // --> Mission: MINE
-    thisCreep.memory.mission = "MINE"
-    console.log("Mineable positions: " + [...availableMiningPositions.keys()])
-    // Select a position available at random and assign it as the mission destination
-    // (RoomPosition object stored in memory)
-    // Select the nearest mineable position available
-    const destination = [...availableMiningPositions.keys()].reduce((a, b) => {
-      const posA = convertRoomPositionStringBackToRoomPositionObject(a)
-      const posB = convertRoomPositionStringBackToRoomPositionObject(b)
-      const rangeToA = thisCreep.pos.getRangeTo(posA)
-      const rangeToB = thisCreep.pos.getRangeTo(posB)
-      return rangeToA < rangeToB ? a : b
-    })
-    thisCreep.memory.destination = destination
-    // Assign the energy source to the mission objective (string resulting from RoomPosition object stored in memory)
-    // Hash key accessed by string lookup of string resulting from RoomPosition
-    thisCreep.memory.objective =
-      availableMiningPositions.get(thisCreep.memory.destination) || null
-    thisCreep.say("⛏️ ASSIGN")
-    console.log(
-      `${thisCreep.name} assigned mission to MINE Objective ${thisCreep.memory.objective} from Destination ${thisCreep.memory.destination}`
-    )
-  }
+  thisCreep.say("⛏️ ASSIGN")
+  thisCreep.memory.mission = "MINE"
+  console.log("Mineable positions: " + [...availableMiningPositions.keys()])
+  // Select the nearest mineable position available
+  const destination = [...availableMiningPositions.keys()].reduce((a, b) => {
+    const posA = convertRoomPositionStringBackToRoomPositionObject(a)
+    const posB = convertRoomPositionStringBackToRoomPositionObject(b)
+    const rangeToA = thisCreep.pos.getRangeTo(posA)
+    const rangeToB = thisCreep.pos.getRangeTo(posB)
+    return rangeToA < rangeToB ? a : b
+  })
+  thisCreep.memory.destination = destination
+  /**
+   * Assign the energy source to the mission objective
+   * (string resulting from RoomPosition object stored in memory)
+   * Hash key accessed by string lookup of string resulting from RoomPosition
+   * */
+  thisCreep.memory.objective = availableMiningPositions.get(destination) || null
+  /**
+   * Remove the selected position from the available positions.
+   *
+   * This will affect the next miner that is assigned a position, i.e. the
+   * mineablePositionsMap will be updated in the main loop.
+   * */
+  availableMiningPositions.delete(destination)
+  console.log(
+    `${thisCreep.name} assigned mission to MINE Objective ${thisCreep.memory.objective} from Destination ${thisCreep.memory.destination}`
+  )
 }
