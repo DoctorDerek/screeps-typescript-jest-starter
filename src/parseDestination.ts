8ad9f4ce0dd28031f61b3204757d6d8c3d279685
Dr. Derek Austin <derekraustin@gmail.com>
2024-08-27 22:03:56 -0600

    fix(logging): remove unnecessary console log message

--- src/parseDestination.ts
+++ src/parseDestination.ts
@@ -5,8 +5,8 @@ export default function parseDestination(thisCreep: Creep) {
   const roomName = groups?.[1] ? groups[1] : null
   const x = groups?.[2] ? Number(groups[2]) : null
   const y = groups?.[3] ? Number(groups[3]) : null
-  console.log(
-    `${thisCreep.memory.destination} parseDestination: ${roomName}, ${x}, ${y}`
-  )
+  // console.log(
+  //   `${thisCreep.memory.destination} parseDestination: ${roomName}, ${x}, ${y}`
+  // )
   return { roomName, x, y }
 }
