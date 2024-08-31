cf3a51082f94b52d7e52f665a57d52c5fdb531aa
Dr. Derek Austin <derekraustin@gmail.com>
2024-08-30 22:05:14 -0600

    feat(priority): claimers and eyes should be n/4

--- src/main.ts
+++ src/main.ts
@@ -645,8 +645,8 @@ function unwrappedLoop() {
       )
     }
     /**
-     * 4 harvester, n/2 miner, n/4 fetcher, n miner, n fetcher, n/2 eye,
-     * n/4 builder, n/4 upgrader, NO defenders x n mining sites in all rooms.
+     * 4 harvester, n/2 miner, n/4 fetcher, n miner, n fetcher, n/4 claimer,
+     * n/4 eye, n/4 builder, n/4 upgrader, NO defenders x n mining sites.
      * 4 harvesters at the beginning all transform into 4 upgraders when the
      * 2nd fetcher is made, and 4 is enough to reach RCL 2 in one go.
      * Builders only spawns if there are construction sites / upgraders if not.
@@ -665,10 +665,13 @@ function unwrappedLoop() {
     else if (fetchers.length < n) spawnFetcher()
     else if (
       energyMax >= 650 &&
-      claimers.length < Math.min(Math.floor(n / 2), numberOfSources)
+      claimers.length <
+        Math.max(Math.floor(n / 4), Math.floor(numberOfSources / 2))
     )
       spawnClaimer()
-    else if (eyes.length < Math.min(Math.floor(n / 2), numberOfSources))
+    else if (
+      eyes.length < Math.max(Math.floor(n / 4), Math.floor(numberOfSources / 2))
+    )
       spawnEye()
     else if (
       !buildArmy &&
